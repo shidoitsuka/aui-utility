3c66c251a0c9a1ccd70575d32f7c7acdfec83343
Brian Harianja <hi@shdev.tech>
2021-09-08 11:21:11 +0800

    added promise catch

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -8,7 +8,11 @@ module.exports = {
     } else {
       reaction = interaction;
     }
-    if (reaction.message.id != "884064323092242482" && !reaction.client.config.devmode) return;
+    if (
+      reaction.message.id != "884064323092242482" &&
+      !reaction.client.config.devmode
+    )
+      return;
     const reactorId = user.id;
     const reactor = await reaction.message.guild.members.fetch(reactorId, {
       force: true,
@@ -20,7 +24,9 @@ module.exports = {
     }
     if (reactor.voice.channel?.id != bot.config.baseVoiceChannelId) {
       reaction.message.channel
-        .send(`<@${reactorId}> Please join <#${bot.config.baseVoiceChannelId}> voice channel first!`)
+        .send(
+          `<@${reactorId}> Please join <#${bot.config.baseVoiceChannelId}> voice channel first!`
+        )
         .then(async (m) => {
           await reaction.users.remove(reactorId);
           await sleep(5000);
@@ -29,15 +35,30 @@ module.exports = {
     } else {
       const voiceChannelId = reactor.voice.channel.id;
       if (voiceChannelId != bot.config.baseVoiceChannelId) return;
-      const name = `🎮┇${bot.config.emojis[reaction.emoji.id].name} - ${reactor.user.tag}`;
-      const baseVoiceChannelPos = bot.channels.cache.get(bot.config.baseVoiceChannelId).position;
-      const createdVoiceChannel = await bot.channels.cache.get(bot.config.baseVoiceChannelId).clone({ name, userLimit: bot.config.emojis[reaction.emoji.id].max });
-      await createdVoiceChannel.setPosition(baseVoiceChannelPos + 1);
-      bot.db.set("voiceChannels", name, createdVoiceChannel.id);
-      // move user to new voice channel
-      await reactor.voice.setChannel(createdVoiceChannel);
-      // delete user reaction
-      await reaction.users.remove(reactorId);
+      const name = `🎮┇${bot.config.emojis[reaction.emoji.id].name} - ${
+        reactor.user.tag
+      }`;
+      const baseVoiceChannelPos = bot.channels.cache.get(
+        bot.config.baseVoiceChannelId
+      ).position;
+      bot.channels.cache
+        .get(bot.config.baseVoiceChannelId)
+        .clone({ name, userLimit: bot.config.emojis[reaction.emoji.id].max })
+        .then((createdVoiceChannel) => {
+          createdVoiceChannel.setPosition(baseVoiceChannelPos + 1);
+          bot.db.set("voiceChannels", name, createdVoiceChannel.id);
+          reactor.voice
+            .setChannel(createdVoiceChannel)
+            .then((_) => {
+              reaction.users
+                .remove(reactorId)
+                .catch((err) => console.error("[REMOVING REACTION]", err));
+            })
+            .catch((err) =>
+              console.error("[MOVING USER TO NEW VOICE CHANNEL]", err)
+            );
+        })
+        .catch((err) => console.error("[CLONING VOICE CHANNEL]", err));
     }
   },
 };
