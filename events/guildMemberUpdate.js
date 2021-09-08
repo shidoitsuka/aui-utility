ec7dbde84adab0df6ed230617fd1040b27937b45
Brian Harianja <hi@shdev.tech>
2021-09-08 08:24:51 +0800

    Update guildMemberUpdate.js
    - removed array of days (unused)
    - changed oldMessage & newMessage to oldMember & newMember
    - added role update feature

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,30 +1,81 @@
 const { MessageEmbed } = require("discord.js");
-const day = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
 
 module.exports = {
   name: "guildMemberUpdate",
-  async execute(oldMessage, newMessage) {
+  async execute(oldMember, newMember) {
     // check if the user changed name
-    if (oldMessage.user.bot) return;
-    if (oldMessage.nickname != newMessage.nickname) {
-      const bot = oldMessage.client;
-      // get old message url
-      const embed = new MessageEmbed()
+    if (oldMember.user.bot) return;
+    const bot = oldMember.client;
+    const embed = new MessageEmbed();
+    if (oldMember.roles.cache.size != newMember.roles.cache.size) {
+      let tmpOldRoles = [];
+      let tmpNewRoles = [];
+      oldMember.roles.cache.map((r) => {
+        if (r.name == "@everyone") return;
+        tmpOldRoles.push(`**${r.name}**`);
+      });
+      newMember.roles.cache.map((r) => {
+        if (r.name == "@everyone") return;
+        tmpNewRoles.push(`**${r.name}**`);
+      });
+      embed
         .setColor("#FFFF00")
-        .setAuthor(`${oldMessage.user.tag} updated their nickname`)
+        .setAuthor(`${oldMember.user.tag} role has been updated`)
         .addFields(
-          { name: "**Old**", value: `${oldMessage.nickname == null ? oldMessage.user.username : oldMessage.nickname}`, inline: true },
-          { name: "**New**", value: `${newMessage.nickname == null ? newMessage.user.username : newMessage.nickname}`, inline: true },
-          { name: "**User ID**", value: `\`${oldMessage.user.id}\``, inline: true },
-          { name: "**Tag**", value: `<@${oldMessage.user.id}>`, inline: true },
+          {
+            name: "**User ID**",
+            value: `\`${oldMember.user.id}\` / <@${oldMember.user.id}>`,
+            inline: false,
+          },
+          tmpOldRoles.length > 0
+            ? {
+                name: "**Old Roles**",
+                value: `${tmpOldRoles.join(", ")}`,
+                inline: true,
+              }
+            : { name: "**Old Roles**", value: "None", inline: true },
+          {
+            name: "**New Roles**",
+            value: `${tmpNewRoles.join(", ")}`,
+            inline: true,
+          }
+        )
+        .setTimestamp();
+      bot.guilds.cache
+        .get(bot.config.logGuildId)
+        .channels.cache.get(bot.db.get("roleUpdateChannel"))
+        .send({ embeds: [embed] })
+        .catch((err) => console.error("[ROLE UPDATE]", err));
+    }
+    if (oldMember.nickname != newMember.nickname) {
+      console.log("NICKNAME UPDATE");
+      embed
+        .setColor("#FFFF00")
+        .setAuthor(`${oldMember.user.tag} updated their nickname`)
+        .addFields(
+          {
+            name: "**User ID**",
+            value: `\`${oldMember.user.id}\` / <@${oldMember.user.id}>`,
+            inline: false,
+          },
+          {
+            name: "**Old**",
+            value: `${
+              oldMember.nickname == null
+                ? oldMember.user.username
+                : oldMember.nickname
+            }`,
+            inline: true,
+          },
+          {
+            name: "**New**",
+            value: `${
+              newMember.nickname == null
+                ? newMember.user.username
+                : newMember.nickname
+            }`,
+            inline: true,
+          }
         )
         .setTimestamp();
       bot.guilds.cache
@@ -33,6 +84,6 @@ module.exports = {
         .send({ embeds: [embed] })
         .catch((err) => console.error("[NICKNAME UPDATE]", err));
     }
-    // oldMsg.channel.send(`<@${oldMsg.author.id}> updated ${oldMsg.content} to ${newMessage.content} in <#${oldMsg.channel.id}> ${oldMsg.url}`);
+    // oldMsg.channel.send(`<@${oldMsg.author.id}> updated ${oldMsg.content} to ${newMember.content} in <#${oldMsg.channel.id}> ${oldMsg.url}`);
   },
 };
