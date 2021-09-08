02005d7a03cec4f80c4a73c45b5c1b9563f8e3af
Brian Harianja <hi@shdev.tech>
2021-09-08 08:23:41 +0800

    Update messageUpdate.js
    fixed mm:ss format at embed footer

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -24,9 +24,9 @@ module.exports = {
     const time = new Date(oldMsg.createdAt);
     const timeString = `[${
       time.getHours() < 10 ? "0" + time.getHours() : time.getHours()
-    }:${time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()}] ${day[time.getDay()]}, ${
-      time.getDate() < 10 ? "0" + time.getDate() : time.getDate()
-    }-${
+    }:${time.getMinutes() ? "0" + time.getMinutes() : time.getMinutes()}] ${
+      day[time.getDay()]
+    }, ${time.getDate() < 10 ? "0" + time.getDate() : time.getDate()}-${
       time.getMonth() + 1 < 10
         ? "0" + (time.getMonth() + 1)
         : time.getMonth() + 1
@@ -37,20 +37,40 @@ module.exports = {
       .setColor("#FFFF00")
       .setAuthor("Updated Message")
       .addFields(
-        { name: "**Original Message**", value: oldMsg.content.length > 1020 ? `${oldMsg.content.substring(0, 1020)}...` : oldMsg.content },
-        { name: "**Edited Message**", value: newMessage.content.length > 1020 ? `${newMessage.content.substring(0, 1020)}...` : newMessage.content },
+        {
+          name: "**Original Message**",
+          value:
+            oldMsg.content.length > 1020
+              ? `${oldMsg.content.substring(0, 1020)}...`
+              : oldMsg.content,
+        },
+        {
+          name: "**Edited Message**",
+          value:
+            newMessage.content.length > 1020
+              ? `${newMessage.content.substring(0, 1020)}...`
+              : newMessage.content,
+        },
         { name: "**Sent by**", value: `<@${oldMsg.author.id}>` },
         { name: "**In channel**", value: `<#${oldMsg.channel.id}>` },
         { name: "**User ID**", value: `\`${oldMsg.author.id}\``, inline: true },
         { name: "**Message ID**", value: `\`${oldMsg.id}\``, inline: true },
-        { name: "**Channel ID**", value: `\`${oldMsg.channel.id}\``, inline: true },
+        {
+          name: "**Channel ID**",
+          value: `\`${oldMsg.channel.id}\``,
+          inline: true,
+        },
         { name: "**Link**", value: `[Click Here](${oldMsgUrl})`, inline: true }
       )
       .setFooter(`Message Timestamp: ${timeString}`)
       .setTimestamp();
     const attachment = newMessage.attachments.first();
     if (attachment) embed.setImage(attachment.url);
-    bot.guilds.cache.get(bot.config.logGuildId).channels.cache.get(bot.db.get("messageUpdateChannel")).send({ embeds: [embed] }).catch(err => console.error("[MESSAGE UPDATE]", err));
+    bot.guilds.cache
+      .get(bot.config.logGuildId)
+      .channels.cache.get(bot.db.get("messageUpdateChannel"))
+      .send({ embeds: [embed] })
+      .catch((err) => console.error("[MESSAGE UPDATE]", err));
     // oldMsg.channel.send(`<@${oldMsg.author.id}> updated ${oldMsg.content} to ${newMessage.content} in <#${oldMsg.channel.id}> ${oldMsg.url}`);
   },
 };
