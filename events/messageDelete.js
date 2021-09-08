305e4acbeae3fb55572e6dc02e750e36d6a25647
Brian Harianja <hi@shdev.tech>
2021-09-08 08:26:19 +0800

    Update messageDelete.js
    - fixed mm:ss format at embed footer
    - logged promise catch as [message delete] instead of update

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -19,9 +19,9 @@ module.exports = {
     const time = new Date(message.createdAt);
     const timeString = `[${
       time.getHours() < 10 ? "0" + time.getHours() : time.getHours()
-    }:${time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()}] ${day[time.getDay()]}, ${
-      time.getDate() < 10 ? "0" + time.getDate() : time.getDate()
-    }-${
+    }:${time.getMinutes() ? "0" + time.getMinutes() : time.getMinutes()}] ${
+      day[time.getDay()]
+    }, ${time.getDate() < 10 ? "0" + time.getDate() : time.getDate()}-${
       time.getMonth() + 1 < 10
         ? "0" + (time.getMonth() + 1)
         : time.getMonth() + 1
@@ -38,13 +38,26 @@ module.exports = {
           inline: true,
         },
         { name: "**Message ID**", value: `\`${message.id}\``, inline: true },
-        { name: "**Channel ID**", value: `\`${message.channel.id}\``, inline: true }
+        {
+          name: "**Channel ID**",
+          value: `\`${message.channel.id}\``,
+          inline: true,
+        }
       )
       .setFooter(`Message Timestamp: ${timeString}`)
       .setTimestamp();
     const attachment = message.attachments.first();
     if (attachment) embed.setImage(attachment.url);
-    if (message.content.length > 0) embed.setDescription(message.content.length > 1024 ? message.content.substring(0, 1021) + "..." : message.content);
-    bot.guilds.cache.get(bot.config.logGuildId).channels.cache.get(bot.db.get("messageDeleteChannel")).send({ embeds: [embed] }).catch(err => console.error("[MESSAGE UPDATE]", err));
+    if (message.content.length > 0)
+      embed.setDescription(
+        message.content.length > 1024
+          ? message.content.substring(0, 1021) + "..."
+          : message.content
+      );
+    bot.guilds.cache
+      .get(bot.config.logGuildId)
+      .channels.cache.get(bot.db.get("messageDeleteChannel"))
+      .send({ embeds: [embed] })
+      .catch((err) => console.error("[MESSAGE DELETE]", err));
   },
 };
